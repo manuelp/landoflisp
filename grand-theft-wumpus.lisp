66f98c38a964dd1bc18fbc2a79a040d365e2dd32
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2011-01-29 22:05:14 +0100

    Funzioni per la creazione alist congestion city e aggiunta polizia

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -68,3 +68,50 @@
 (defun connect-all-islands (nodes edge-list)
   (append (connect-with-bridges (find-islands nodes edge-list))
 	  edge-list))
+
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num*
+		      collect i))
+	 (edge-list (connect-all-islands nodes (make-edge-list)))
+	 (cops (remove-if-not (lambda (x)
+				(zerop (random *cop-odds*)))
+			      edge-list)))
+    (add-cops (edges-to-alist edge-list) cops)))
+
+; Converte la lista di connessioni in una alist (doppia) a cui ad ogni
+; nodo è associata una serie di liste ad un solo elemento, il quale
+; rappresenta un nodo raggiungibile direttamente da quello
+; considerato.
+(defun edges-to-alist (edge-list)
+  (mapcar (lambda (node1)
+	    (cons node1
+		  (mapcar (lambda (edge)
+			    (list (cdr edge)))
+			  (remove-duplicates (direct-edges node1 edge-list)
+					     :test #'equal))))
+	  (remove-duplicates (mapcar #'car edge-list))))
+
+; mapcar sui nodi, poi mapcar interno sui nodi connessi ad ognuno. Nel
+; mapcar più interno costruisce un edge-pari per il nodo sorgente e
+; destinazione e controlla l'eventuale intersezione con la lista delle
+; connessioni con la polizia. Se c'è allora crea una lista con il nodo
+; e il simbolo 'COPS, altrimenti ritorna direttamente la lista singola
+; con il nodo di destinazione (es. (2)).
+;
+; alist: ((1 (2)) (2 (1) (3)) (3 (2)))
+; cops: (2 . 1)
+; result: ((1 (2)) (2 (1 COPS) (3)) (3 (2)))
+(defun add-cops (edge-alist edges-with-cops)
+  (mapcar (lambda (x)
+	    (let ((node1 (car x))
+		  (node1-edges (cdr c)))
+	      (cons node1
+		    (mapcar (lambda (edge)
+			      (let ((node2 (car edge)))
+				(if (intersection (edge-pair node1 node2)
+						  edges-with-cops
+						  :test #'equal)
+				    (list node2 'cops)
+				  edge)))
+			    node1-edges))))
+	  edge-alist))
