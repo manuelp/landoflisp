daa2879c0fed033fd4b4425e458109e35805904e
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2011-01-30 17:00:07 +0100

    Terminato GTW

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -149,13 +149,6 @@
 			  (when (some #'cdr (cdr (assoc n edge-alist)))
 			    '(sirens!))))))
 
-(defun new-game ()
-  (setf *congestion-city-edges* (make-city-edges))
-  (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
-  (setf *player-pos* (find-empty-node))
-  (setf *visited-nodes* (list *player-pos*))
-  (draw-city))
-
 (defun find-empty-node ()
   (let ((x (random-node)))
     (if (cdr (assoc x *congestion-city-nodes*))
@@ -164,3 +157,77 @@
 
 (defun draw-city ()
   (ugraph->png "city" *congestion-city-nodes* *congestion-city-edges*))
+
+; Crea una lista parziale di nodi di congestion city a partire dalla posizione
+; attuale. Mostra solo i nodi direttament e raggiungibili con una
+; connessione diretta e senza informazioni sugli attributi di quei
+; nodi.
+(defun known-city-nodes ()
+  (mapcar (lambda (node)
+	    (if (member node *visited-nodes*)
+		(let ((n (assoc node *congestion-city-nodes*)))
+		  (if (eql node *player-pos*)
+		      (append n '(*))
+		    n))
+	      (list node '?)))
+	  (remove-duplicates
+	   (append *visited-nodes*
+		   (mapcan (lambda (node)
+			     (mapcar #'car
+				     (cdr (assoc node *congestion-city-edges*))))
+			   *visited-nodes*)))))
+
+; Crea le connessioni parziali tra i nodi visitati e quelli
+; direttamente connessi. Conserva gli attributi della connessione
+; (cops) solo se sia il nodo sorgente che di destinazione sono stati
+; visitati.
+(defun known-city-edges ()
+  (mapcar (lambda (node)
+	    (cons node (mapcar (lambda (x)
+				 (if (member (car x) *visited-nodes*)
+				     x
+				   (list (car x))))
+			       (cdr (assoc node *congestion-city-edges*)))))
+	  *visited-nodes*))
+
+(defun draw-known-city ()
+  (ugraph->png "know-city" (known-city-nodes) (known-city-edges)))
+
+(defun new-game ()
+  (setf *congestion-city-edges* (make-city-edges))
+  (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
+  (setf *player-pos* (find-empty-node))
+  (setf *visited-nodes* (list *player-pos*))
+  (draw-city)
+  (draw-known-city))
+
+(defun walk (pos)
+  (handle-direction pos nil))
+
+(defun charge (pos)
+  (handle-direction pos t))
+
+(defun handle-direction (pos charging)
+  (let ((edge (assoc pos (cdr (assoc *player-pos* *congestion-city-edges*)))))
+    (if edge
+	(handle-new-place edge pos charging)
+      (princ "That location does not exist!"))))
+
+(defun handle-new-place (edge pos charging)
+  (let* ((node (assoc pos *congestion-city-nodes*))
+	 (has-worm (and (member 'glow-worm node)
+			(not (member pos *visited-nodes*)))))
+    (pushnew pos *visited-nodes*)
+    (setf *player-pos* pos)
+    (draw-known-city)
+    (cond ((member 'cops edge) (princ "You rand into the cops. Game Over."))
+	  ((member 'wumpus node) (if charging
+				     (princ "You found the wumpus!")
+				   (princ "You ran into the Wumpus. Game Over.")))
+	  (charging (princ "You wasted your last bullet. Game Over."))
+	  (has-worm (let ((new-pos (random-node)))
+		      (princ "You ran into a Glow Worm Gang! You're now at ")
+		      (princ new-pos)
+		      (handle-new-place nil new-pos nil))))))
+			 
+; TODO Il gioco continua anche con il game over
